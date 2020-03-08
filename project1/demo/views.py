fa74bef79b4e721a4e2d06292c9149bcc67d6505
Prince Raj <princeraj427@gmai.com>
2020-03-08 10:39:19 +0530

    Django RestFramework added

---
+++ project1/demo/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from .serializers import BookSerializer, BookMiniSerializer
+from .models import Book
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response 
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookMiniSerializer
+    queryset = Book.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        #serializer = self.get_serializer(instance)
+        serializer = BookSerializer(instance)
+        return Response(serializer.data)
